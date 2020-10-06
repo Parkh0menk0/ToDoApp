f5bc86848b1e7e44bf403ef7bc42761739f8d4e7
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2020-10-06 23:37:21 +0300

    Разбивает стартовый элемент приложения на компоненты

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const el = (
-  <div>
-    <h1>My Todo List</h1>
-    <input placeholder="search" />
+const AppHeader = () => {
+  return <h1>My Todo List</h1>;
+};
+
+const SearchPanel = () => {
+  return <input placeholder="search" />;
+};
+
+const TodoList = () => {
+  return (
     <ul>
       <li>Learn React</li>
       <li>Build awesome App</li>
     </ul>
-  </div>
-);
+  );
+};
 
-ReactDom.render(el, document.getElementById("root"));
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
