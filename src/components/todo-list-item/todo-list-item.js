c8c48217f277d07c4c7541e3a86b70486f1a726f
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2020-10-08 23:32:39 +0300

    Добавляет отлов и получение события click по лейблу задачи

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,32 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './todo-list-item.css';
+import "./todo-list-item.css";
 
 export default class TodoListItem extends Component {
+  constructor() {
+    super();
+
+    this.onLabelClick = () => {
+      console.log(this.props.label);
+    };
+  }
 
   render() {
     const { label, important = false } = this.props;
 
     const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
+      color: important ? "steelblue" : "black",
+      fontWeight: important ? "bold" : "normal",
     };
-  
+
     return (
       <span className="todo-list-item">
-        <span
-          className="todo-list-item-label"
-          style={style}>
+        <span className="todo-list-item-label" style={style} onClick={this.onLabelClick}>
           {label}
         </span>
-  
-        <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
+
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm float-right"
+        >
           <i className="fa fa-exclamation" />
         </button>
-  
-        <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
+
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm float-right"
+        >
           <i className="fa fa-trash-o" />
         </button>
       </span>
