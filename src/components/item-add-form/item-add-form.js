651724d87e0cfe500ccfd93740cdb659d4800506
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2020-10-12 01:00:04 +0300

    Добавляет возможность ввода не предопределённых задач

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,14 +3,35 @@ import React, { Component } from "react";
 import "./item-add-form.css";
 
 export default class ItemAddForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      label: "",
+    };
+
+    this.onLabelChange = (evt) => this.setState({ label: evt.target.value });
+    this.onFormSubmit = (evt) => {
+      evt.preventDefault();
+      this.props.addItem(this.state.label)
+    };
+  }
 
   render() {
-    const { addItem } = this.props;
-
     return (
-      <div className="item-add-form">
-        <button className="btn btn-outline-secondary" type="button" onClick={() => addItem("Hello!")}>Add item</button>
-      </div>
+      <form className="item-add-form d-flex" onSubmit={this.onFormSubmit}>
+        <input
+          className="form-control"
+          type="text"
+          onChange={this.onLabelChange}
+          placeholder="What needs to be done?"
+        />
+        <button
+          className="btn btn-outline-secondary"
+        >
+          Add item
+        </button>
+      </form>
     );
   }
 }
